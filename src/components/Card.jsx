43b67203055b29e8e30d5a963d763d50afd67b14
Rawle Juglal <rawledjuglal@gmail.com>
2022-08-03 21:26:29 -0500

    first pass
    created skeleton and components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,28 @@
 import react from 'react'
 import reactLogo from '../assets/react.svg'
+import CardFooter from './CardFooter/CardFooter'
 
 export default function Card(){
     return(
         <div className="card">
-            <img src={reactLogo} class="card-img-top" alt="..." />
+            <img src="https://res.cloudinary.com/relentlessrawle/image/upload/v1659558095/RJ_FencePost_ht0j37.jpg" class="card-img-top" alt="..." />
             <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <div className="--card-body-container">
+                    <h1 className="--card-title">Rawle Juglal</h1>
+                    <p className="--card-profession">Frontend Developer</p>
+                    <p className="--card-site">rawlejuglal.me</p>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary" type="button">Email</button>
+                    </div>
+                    <h2>About</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2>Interest</h2>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                
             </div>
             <div className="card-footer">
-                Card footer
+                <CardFooter />
             </div>
         </div>
     )
